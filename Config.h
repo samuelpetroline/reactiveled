4b44b875e9ac849406af914157ba17fdab443619
Samuel Gonçalves Petrsaoline <samuel.petroline@fatec.sp.gsav.br>
2018-11-25 17:25:48 -0200

    Exclusão de arquivos desnecessários

--- Config.h
+++
@@ -1,7 +0,0 @@
-#define NUM_LEDS 200
-#define DATA_PIN 5
-
-#define SAMPLES 128             //Must be a power of 2
-#define SAMPLING_FREQUENCY 1000 //Hz, must be less than 10000 due to ADC
-
-CRGB leds[NUM_LEDS];
